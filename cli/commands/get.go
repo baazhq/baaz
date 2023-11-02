2a4850252fbf389c64590551a60857d24eaecbb5
AdheipSingh <34169002+AdheipSingh@users.noreply.github.com>
2023-11-03 01:01:45 +0530

    add dataplane command (#26)

--- cli/commands/get.go
+++ cli/commands/get.go
@@ -7,25 +7,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var getValidArgs = []string{
-	"customers",
-	"dataplanes",
-}
-
 var (
 	getCmd = &cobra.Command{
 		Use:       "get",
 		Short:     "bz get - list entites [Customers, Dataplane, Tenants, Applications] in baaz control plane",
 		Args:      cobra.ExactArgs(1),
-		ValidArgs: getValidArgs,
+		ValidArgs: commonValidArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			switch args[0] {
-			case "customers":
+			case "customers", "customer":
 				return customers.GetCustomers()
 			case "dataplanes":
 				return dataplanes.GetDataplanes()
 			default:
-				return NotValidArgs(getValidArgs)
+				return NotValidArgs(commonValidArgs)
 			}
 		},
 	}
