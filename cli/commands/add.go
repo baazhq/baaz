7dc8cd084acacebe7571443079c619b04a431b71
Pulak Kanti Bhowmick <pkbhowmick007@gmail.com>
2024-09-03 12:42:59 +0600

    use flag instead of args (#99)
    Signed-off-by: Pulak Kanti Bhowmick <pkbhowmick007@gmail.com>

--- cli/commands/add.go
+++ cli/commands/add.go
@@ -11,12 +11,18 @@ var (
 	addCmd = &cobra.Command{
 		Use:   "add",
 		Short: "bz add - add a customer to existing dataplane in baaz control plane",
-		Args:  cobra.ExactArgs(3),
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 
 			switch args[0] {
 			case "dataplane", "dataplanes":
-				resp, err := dataplanes.AddDataplane(args[1], args[2])
+				if dataplane_name == "" {
+					return fmt.Errorf("dataplane name can't be empty, use flag --dataplane={dataplane_name}")
+				}
+				if customer_name == "" {
+					return fmt.Errorf("customer name can't be empty, use flag --customer={customer_name}")
+				}
+				resp, err := dataplanes.AddDataplane(dataplane_name, customer_name)
 				if err != nil {
 					return err
 				}
@@ -32,4 +38,6 @@ var (
 
 func init() {
 	rootCmd.AddCommand(addCmd)
+	addCmd.Flags().StringVarP(&dataplane_name, "dataplane", "", "", "dataplane name")
+	addCmd.Flags().StringVarP(&customer_name, "customer", "", "", "customer name")
 }
